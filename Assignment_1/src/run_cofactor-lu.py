8287f67551dd36ace9d6f2f1476c8576f146842e
Parikshit Bajpai <parikshit.bajpai@uoit.net>
2019-10-04 16:14:33 -0400

    Closes #3. Updated tex files

--- Assignment_1/src/run_cofactor-lu.py
+++ Assignment_1/src/run_cofactor-lu.py
@@ -17,7 +17,7 @@ from Cofactor import determinant_cofactor
 ntrials = 6                                         # Number of trials
 wtime = np.zeros((ntrials,3))                       # Array to store wall times
 error_rel = np.zeros((ntrials,3))                   # Array to store relative errors
-error_absolute = np.zeros((ntrials,2))              # Array to store difference in errors
+error_absolute = np.zeros((ntrials,4))              # Array to store difference in errors
 n=2                                                 # Initial matrix size
 
 for k in range(0,ntrials):
@@ -34,12 +34,14 @@ for k in range(0,ntrials):
     det_cofactor = determinant_cofactor(A)
     elapsed_cofactor = time.time()-start_cofactor   # Elapsed time using LUP
 
-    ## Difference between determinant computed from LU decomposition and Cofactor methods.
-    error_absolute[k,0] = n
-    error_absolute[k,1] = abs(det_lu - det_cofactor)
-
     ## The following commands use numpy to compute determinant of A and is assumed to be the "correct" value of determinant.
     det_np = np.linalg.det(A)
+
+    ## Difference between determinant computed from LU decomposition and Cofactor methods.
+    error_absolute[k,0] = n
+    error_absolute[k,1] = abs(det_lu - det_np)
+    error_absolute[k,2] = abs(det_cofactor - det_np)
+    error_absolute[k,3] = abs(det_lu - det_cofactor)
 
     ## The relative errors are computed with respect to determinant using numpy which is assumed to be the 'true' value.
     error_rel_lu = abs(det_lu-det_np)/abs(det_np)           # Rel Error using LUP
@@ -77,10 +79,11 @@ plt.legend(('Wall Time: LUP','Wall Time: Cofactor','Order of Complexity: n^3','O
 plt.show()
 
 # This plot shows the difference in the values of determinants computed using the two methods.
-plt.loglog(error_absolute[:,0],error_absolute[:,1],'-o')
-plt.title('Difference in determinant vs. matrix size')
+plt.loglog(error_absolute[:,0],error_absolute[:,1],'-o',error_absolute[:,0],error_absolute[:,2],'-o',error_absolute[:,0],error_absolute[:,3],'-*')
+plt.title('Absolute errors vs. matrix size')
 plt.xlabel('Matrix size [n]')
-plt.ylabel('Difference [unitless]')
+plt.ylabel('Absolute error [unitless]')
+plt.legend(('Absolute error: LUP','Absolute error: Cofactor','Difference'))
 plt.show()
 
 ## This plot shows the relative errors with respect to 'true' determinant computed using numpy.
--- Assignment_1/src/run_lu.py
+++ Assignment_1/src/run_lu.py
@@ -12,13 +12,15 @@ import matplotlib.pylab as plt
 from LUP import LUP
 from Determinant_LUP import determinant_LUP
 
-ntrials = 10                                         # Number of trials
+ntrials = 8                                         # Number of trials
 wtime = np.zeros((ntrials,2))                       # Array to store wall time
 error_rel = np.zeros((ntrials,2))                   # Array to store relative error
+error_absolute = np.zeros((ntrials,2))              # Array to store absolute error
+condition_number = np.zeros((ntrials,2))            # Array to store condition number
 n=2                                                 # Initial matrix size
 
 for k in range(0,ntrials):
-    A = np.random.rand(n,n)
+    A = np.random.rand(n,n)                         # Generate random matrix
 
     ## The following commands use the LUP decomposition to compute determinant of A.
     start_lu = time.time()                          # Get start time for LUP
@@ -26,27 +28,33 @@ for k in range(0,ntrials):
     det_lu = determinant_LUP(L)*determinant_LUP(U)*par
     elapsed_lu = time.time()-start_lu               # Elapsed time using LUP
 
+    # Print wall time to screen
+    print('Size = %d, Time_LU = %e' % (n,elapsed_lu))
+
     ## The following commands use numpy to compute determinant of A and is assumed to be the "correct" value of determinant.
     det_np = np.linalg.det(A)
 
+    ## Condition number of the matrix.
+    condition_number[k,0] = n
+    condition_number[k,1] = np.linalg.cond(A,2)
+
+    ## The absolute errors are computed with respect to determinant using numpy which is assumed to be the 'true' value.
+    error_absolute[k,0] = n
+    error_absolute[k,1] = abs(det_lu-det_np)
+
     ## The relative errors are computed with respect to determinant using numpy which is assumed to be the 'true' value.
-    error_rel_lu = abs(det_lu-det_np)/abs(det_np)           # Rel Error using LUP
+    error_rel[k,0] = n
+    error_rel[k,1] = abs(det_lu-det_np)/abs(det_np)
 
-    print('Size = %d, Time_LU = %e' % (n,elapsed_lu))
-
-    ## The following parts are used to save the results for time and error.
-    wtime[k,0] = n                                          # Wall times
+    ## The following array are used to save the results for time.
+    wtime[k,0] = n
     wtime[k,1] = elapsed_lu
-
-    ## The relative errors are computed with respect to determinant using numpy which is assumed to be the 'true' value.
-    error_rel[k,0] = n                                      # Relative errors
-    error_rel[k,1] = error_rel_lu
 
     ## Increase matrix size for subsequent trial.
     n *= 2
 
 ## Arrays to plot the orders of complexity
-x = np.arange(1024)                                          # Array of natural numbers to plot complexity
+x = np.arange(256)                                          # Array of natural numbers to plot complexity
 y_LU = [math.pow(i,3) for i in x]                           # Complexity of LU decomposition
 
 ## The following commands plot the relevant graphs
@@ -55,9 +63,18 @@ plt.loglog(wtime[:,0],wtime[:,1],'-o',y_LU,'--')
 plt.title('Computation time vs. matrix size')
 plt.xlabel('Matrix size [n]')
 plt.ylabel('Time [s]')
-plt.xlim([2,1024])
+plt.xlim([2,256])
 plt.ylim(bottom=0.1/10000000,top=100000000)
 plt.legend(('Wall Time: LUP','Order of Complexity: n^3'), loc='upper left')
+plt.show()
+
+# This plot shows the absolute error with respect to 'true' determinant computed using numpy.
+plt.loglog(error_absolute[:,0],error_absolute[:,1],'-o',condition_number[:,0],condition_number[:,1],'--')
+plt.title('Absolute errors vs. matrix size')
+plt.xlabel('Matrix size [n]')
+plt.ylabel('Absolute error [unitless]')
+plt.xlim([2,256])
+plt.legend(('Absolute error: LUP','Condition number'))
 plt.show()
 
 ## This plot shows the relative errors with respect to 'true' determinant computed using numpy.
@@ -65,5 +82,4 @@ plt.loglog(error_rel[:,0],error_rel[:,1],'-o')
 plt.title('Relative errors vs. matrix size')
 plt.xlabel('Matrix size [n]')
 plt.ylabel('Relative error [unitless]')
-plt.legend(('Relative error: LUP'))
 plt.show()
